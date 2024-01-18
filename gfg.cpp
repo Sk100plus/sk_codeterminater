2e8a833a11199d3385162a5d83cf2fed8b6d8533
Suraj Kasaudhan <kasaudhansuraj25@gmail.com>
2024-01-18 22:32:41 +0530

    third

--- gfg.cpp
+++ gfg.cpp
@@ -1,15 +1,55 @@
-#include<bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
 using namespace std;
-int main(){
-   map<int,int> up;
-   up[4]=1;
-   
-   up[-5]=1;
-   up[1]=1;
- map <int,int> :: iterator it=up.begin();
-   while(it!=up.end()){
-    cout<<it->first<<" "<<it->second<<endl;
-    it++;
-   }
+
+int countTriangles(vector<int>& sticks) {
+    sort(sticks.begin(), sticks.end());
+
+    int count = 0;
+    int n = sticks.size();
+
+    // Initialize a 2D DP array with zeros
+    vector<vector<int>> dp(n, vector<int>(n, 0));
+
+    for (int i = n - 3; i >= 0; --i) {
+        int k = i + 2;
+
+        for (int j = i + 1; j < n - 1; ++j) {
+            while (k < n && sticks[i] + sticks[j] > sticks[k]) {
+                ++k;
+            }
+
+            // Update DP table
+            dp[i][j] = max(0, k - j - 1);
+
+            // Accumulate count
+            count += dp[i][j];
+        }
+    }
+
+    return count;
+}
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n;
+        cin >> n;
+
+        vector<int> vec(n);
+        vector<int> stick(n);
+
+        for (int i = 0; i < n; i++) {
+            cin >> vec[i];
+            stick[i] = 1 << vec[i];
+        }
+
+        cout << countTriangles(stick) << endl;
+    }
+
     return 0;
 }
